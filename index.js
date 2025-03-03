76c80279b0cd02cfefba01e4610ab749b3f7e145
Ian Wafula <neocorpse@proton.me>
2025-03-03 16:19:05 +0300

    Create relevant functions to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    return arr.slice(0,2)
+}
+
+function returnLastTwoDrivers(arr) {
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return fare * num
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareQuintupler = createFareMultiplier(4)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(arr, fun) {
+    return fun(arr)
+}
